068ca662f2df6f40511fa84b29c05eee2846469e
Drew-Andersen <andrewtandersen@gmail.com>
2025-02-11 18:25:25 -0500

    Added the game logic and Player class

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # War-Card-Game
-War - Created in Python
+
+## Images
+<!-- ![Start Screen](<./images/start-screen.png>) -->
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Description
+This application is a card war game. War is a card game that players will receive 1/2 a deck of cards, and play the top card each round. Whoever has the highest card wins the hand, but if the cards are equal in value (rank) there will be a war. In a war, players will each put the 3 cards at the top of their deck into a pile and then flip the next card to see who has the highest ranking card. The player with the higher ranking card will win the hand and the cards. The object of the game is to make the other player run out of cards.
+
+## Installation
+1. Clone down the repository to your local machine. You can use the command:
+'git clone https://github.com/drew-andersen/ar-Card-Game.git
+
+2. Open VS Code or other IDE of your choice.
+
+3. Right click on the file war.py and click on 'Run Python File in Terminal'
+
+
+## Usage
+When the application is run, the game will auto run to see who will win the game. If there is a draw in war, the computer will win the hand.
+
+## License
+![MIT License badge](<./images/MIT-badge.png>)
+
+## Credits
+Drew Andersen
+[Github](https://github.com/drew-andersen)
--- war.py
+++ war.py
@@ -5,7 +5,7 @@ RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
 class Deck:
     def __init__(self):
-        self.allcards = [(suite, rank) for suite in SUITES for rank in RANKS]
+        self.allcards = [(s, r) for s in SUITES for r in RANKS]
         print("Creating a new deck")
     
     def shuffle(self):
@@ -23,16 +23,92 @@ class Hand:
         return 'Contains {} cards'.format(len(self.cards))
     
     def add(self, added_cards):
-        self.cards.extend(added_cards)
+        return self.cards.extend(added_cards)
 
     def remove_cards(self):
-        self.cards.pop()
+        return self.cards.pop()
 
 class Player:
-    '''
-    This will take in a name and an instance of Hand. The player can 
-    then play cards and check if they still have cards.
-    '''
-    pass
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+
+    def play_card(self):
+        drawn_card = self.hand.remove_cards()
+        print('{} has placed: {}'.format(self.name, drawn_card))
+        print('')
+        return drawn_card
+    
+    def remove_war_cards(self):
+        war_cards = []
+        if len(self.hand.cards) >= 3:
+            for x in range(3):
+                war_cards.append(self.hand.cards.pop())
+            return war_cards
+        elif len(self.hand.cards) >= 2 and len(self.hand.cards) < 3:
+            for x in range(2):
+                war_cards.append(self.hand.cards.pop())
+            return war_cards
+        else:
+            war_cards.append(self.hand.cards.pop())
+            return war_cards
+
+    
+    def have_cards(self):
+        if len(self.hand.cards) != 0:
+            return True    
 
 # Game Play #
+# Create the deck
+deck = Deck()
+deck.shuffle()
+half1, half2 = deck.split_deck()
+
+# Create both players
+computer = Player('computer', Hand(half1))
+player = input("Enter your name: ")
+user = Player(player, Hand(half2))
+
+ttl_rounds = 0
+war_count = 0
+
+while user.have_cards() and computer.have_cards():
+    ttl_rounds += 1
+    print("Start a new round!")
+    print('Current Standings: ')
+    print(user.name + 'has hte count: ' + str(len(user.hand.cards)))
+    print(computer.name + 'has hte count: ' + str(len(computer.hand.cards)))
+    print('Play a card!')
+    print('')
+
+    table_cards = []
+
+    comp_card = computer.play_card()
+    user_card = user.play_card()
+
+    table_cards.append(comp_card)
+    table_cards.append(user_card)
+
+    if comp_card[1] == user_card[1]:
+        war_count += 1
+        print("WAR!")
+        table_cards.extend(user.remove_war_cards())
+        table_cards.extend(computer.remove_war_cards())
+
+        if RANKS.index(comp_card[1]) < RANKS.index(user_card[1]):  # Change comparison here
+            user.hand.add(table_cards)
+        else:
+            computer.hand.add(table_cards)
+    else:
+        if RANKS.index(comp_card[1]) < RANKS.index(user_card[1]):  # Change comparison here
+            user.hand.add(table_cards)
+        else:
+            computer.hand.add(table_cards)
+    
+print('GAME OVER! Number of rounds: ' + str(ttl_rounds))
+print('A war happened ' + str(war_count) + ' times.')
+
+if computer.have_cards() == True:
+    print('Computer Wins!')
+else:
+    print('Player Wins!')
